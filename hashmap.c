b1f21786dc9168db0353bc12d820645bcc977231
KNB5 <163059413+KNB5@users.noreply.github.com>
2024-04-23 20:51:09 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -75,8 +75,6 @@ void eraseMap(HashMap * map,  char * key) {
 
 Pair * searchMap(HashMap * map,  char * key) 
 {   
-  if(map==NULL || key==NULL) return NULL;
-  long pos = hash(key,map->capacity);
   
 }
 
