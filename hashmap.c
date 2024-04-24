cfd44ba0f2920d761c47d30bb68854ffae2962b8
KNB5 <163059413+KNB5@users.noreply.github.com>
2024-04-24 20:13:09 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -99,9 +99,17 @@ HashMap * createMap(long capacity)
 void eraseMap(HashMap * map,  char * key) 
 {    
   if(map==NULL || key==NULL) return;
-  long pos = hash(key, map->capacity);
-  map->buckets[pos]->key = NULL;
-  map->size--;
+  int pos = hash(key, map->capacity);
+  while(map->buckets[pos] != NULL)
+    {
+      if(is_equal(map->buckets[pos]->key, key))
+      {
+        map->buckets[pos]->key = NULL;
+        map->size--;
+        return;
+      }
+      pos = (pos+1)%map->capacity;
+    }
 }
 
 Pair * searchMap(HashMap * map,  char * key) 
